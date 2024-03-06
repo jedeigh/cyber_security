03df787ec228e30e17f3f0332665c708adb0a67e
JR Heru <jedeigh.heru@gmail.com>
2024-03-05 19:43:50 -0600

    challenge2.sh

--- challenge2.sh
+++ challenge2.sh
@@ -1,5 +1,5 @@
 # #!bin/bash
 # Declare a variable with as your name
-x=JH
+y=JH
 # Print to the screen Welcome to the Class Name(variable)
-echo "Welcome to the Class $x"
+echo "Welcome to the Class $y"
