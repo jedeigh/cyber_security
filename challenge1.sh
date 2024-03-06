bc9244b10a9f45220bda04fddce97f7cca2b2ef3
jrheru <jedeigh.heru@gmail.com>
2024-03-06 05:29:24 -0600

    "another commit"

---
+++ challenge1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Script: Ops 201 Class 01 Ops Challenge Solution 
+# Author: Jedeigh Heru
+# Date of latest revision 2/27/24
+# Purpose: Print a string to the terminal
+echo "Hello World!"
