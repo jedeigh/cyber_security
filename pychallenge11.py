c04ee8b5a4f6b31f6583ee2642249616803c6003
jedeigh <jedeigh.heru@gmail.com>
2024-05-07 18:40:38 -0500

    11 challenge update

--- pychallenge11.py
+++ pychallenge11.py
@@ -3,9 +3,11 @@
 #Can you randomize your password generated
 
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import string 
+
+letters = string.ascii_letters
+digits = string.digits
+specials = string.punctuation
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
@@ -21,13 +23,13 @@ for _ in range(nr_letters):
 
 # Generating random symbols
 for _ in range(nr_symbols):
-    random_index = random.randint(0, len(symbols) - 1)
-    password.append(symbols[random_index])
+    random_index = random.randint(0, len(specials) - 1)
+    password.append(specials[random_index])
 
 # Generating random numbers
 for _ in range(nr_numbers):
-    random_index = random.randint(0, len(numbers) - 1)
-    password.append(numbers[random_index])
+    random_index = random.randint(0, len(digits) - 1)
+    password.append(digits[random_index])
 
 # Shuffling the password to randomize it
 random.shuffle(password)
